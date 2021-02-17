9706de136773c8fa8374039d949f7d0952de26cc
Artem <40913z@mail.ru>
2021-02-18 02:24:40 +0300

    completed hw-3

--- lesson_3/src/main/java/com/temzu/spring/mvc/controllers/ProductController.java
+++ lesson_3/src/main/java/com/temzu/spring/mvc/controllers/ProductController.java
@@ -1,10 +1,53 @@
 package com.temzu.spring.mvc.controllers;
 
+import com.temzu.spring.mvc.model.Product;
+import com.temzu.spring.mvc.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-@RequestMapping("/products")
 public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/index")
+    public String homePage() {
+        return "index";
+    }
+
+    @GetMapping("/products")
+    public String getAllProducts(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
+        return "products";
+    }
+
+    @GetMapping("/products/remove/{id}")
+    public String removeById(@PathVariable("id") Long id) {
+        productService.deleteProductById(id);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/info/{id}")
+    public String detailsPage(Model model, @PathVariable("id") Long id) {
+        Product product = productService.getProductById(id);
+        model.addAttribute("selectedProduct", product);
+        return "info";
+    }
+
+    @PostMapping("/products/add")
+    public String addNewProduct(@ModelAttribute Product product) {
+        productService.save(product);
+        return "redirect:/products";
+    }
 
 }
--- lesson_3/src/main/java/com/temzu/spring/mvc/repositories/ProductRepository.java
+++ lesson_3/src/main/java/com/temzu/spring/mvc/repositories/ProductRepository.java
@@ -1,5 +1,42 @@
 package com.temzu.spring.mvc.repositories;
 
+import com.temzu.spring.mvc.model.Product;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.servlet.ServletException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
 public class ProductRepository {
+    private List<Product> products;
+
+    @PostConstruct
+    public void init() throws ServletException {
+        products = new ArrayList<>();
+        products.add(new Product(1L, "Milk", 50));
+        products.add(new Product(2L, "Meat", 200));
+        products.add(new Product(3L, "Watter", 30));
+        products.add(new Product(4L, "Fish", 1500));
+        products.add(new Product(5L, "Egg", 62));
+        products.add(new Product(6L, "Tea", 120));
+        products.add(new Product(7L, "Rice", 73));
+        products.add(new Product(8L, "Cabbage", 22));
+        products.add(new Product(9L, "Apple", 100));
+        products.add(new Product(10L, "Orange", 90));
+    }
+
+    public List<Product> getProducts() {
+        return Collections.unmodifiableList(products);
+    }
+
+    public void removeById(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+
+    public void save(Product product) {
+        products.add(product);
+    }
 }
