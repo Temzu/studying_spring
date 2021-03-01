6fc6e6e78c7e8c62c2b5f277ea1f98eaeb07b2a5
Artem <40913z@mail.ru>
2021-03-01 22:18:49 +0300

    3 task

--- jpa_hiber_part-2/src/main/java/com/temzu/jpa_hiber/entities/Customer.java
+++ jpa_hiber_part-2/src/main/java/com/temzu/jpa_hiber/entities/Customer.java
@@ -2,11 +2,42 @@ package com.temzu.jpa_hiber.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "customers")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "customers_products",
+            joinColumns = @JoinColumn(name = "customer_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Customer [id = %d, name = %s]", id, name);
+    }
 }
--- jpa_hiber_part-2/src/main/java/com/temzu/jpa_hiber/entities/Product.java
+++ jpa_hiber_part-2/src/main/java/com/temzu/jpa_hiber/entities/Product.java
@@ -1,4 +1,46 @@
 package com.temzu.jpa_hiber.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "cost")
+    private int cost;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "customers_products",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "customer_id")
+    )
+    private List<Customer> customers;
+
+    public Product(String title, int cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product [id = %d, title = %s, cost = %d]", id, title, cost);
+    }
 }
